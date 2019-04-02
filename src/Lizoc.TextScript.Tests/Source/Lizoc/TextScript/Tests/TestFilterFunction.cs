5d21ab3d55291e90968163f61e79643670d5a8a8
imacks <macksme@outlook.com>
2019-04-02 12:39:25 +0800

    fixed header typo in TestFilterFunction.cs

--- src/Lizoc.TextScript.Tests/Source/Lizoc/TextScript/Tests/TestFilterFunction.cs
+++ src/Lizoc.TextScript.Tests/Source/Lizoc/TextScript/Tests/TestFilterFunction.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="TestParser.cs" repo="TextScript">
+// <copyright file="TestFilterFunction.cs" repo="TextScript">
 //     Copyright (C) 2018 Lizoc Inc. <http://www.lizoc.com>
 //     The source code in this file is subject to the MIT license.
 //     See the LICENSE file in the repository root directory for more information.
